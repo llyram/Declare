c8a250af59a43314fef74d298a9c56bf4a33da21
Mister-H <hs913271@gmail.com>
2022-10-01 16:22:15 +0530

    Changed Profile Icons with Dicebear avatar icons using Dicebear API
    HACKTOBERFEST

--- frontend/src/components/ProfileCircle.js
+++ frontend/src/components/ProfileCircle.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 const ProfileCircle = ({ name, currentName, color }) => {
+    const dicebearApi = 'https://avatars.dicebear.com/api/pixel-art/';
+
     return (
         <div className="flex-centered-column profilecirclediv">
             <div className='profilecirclecontainer'>
                 {name === currentName ?
-                    (<div className="profilecircle flex-centered currentturn" style={{backgroundColor: color}}>{name.substring(0, 1).toUpperCase()}</div>)
+                    (<div className="profilecircle flex-centered currentturn" style={{backgroundColor: color}}><img src={dicebearApi + name + '.svg'} alt={name} /></div>)
                     :
-                    (<div className="profilecircle flex-centered" style={{backgroundColor: color}}>{name.substring(0, 1).toUpperCase()}</div>)
+                    (<div className="profilecircle flex-centered" style={{backgroundColor: color}}><img src={dicebearApi + name + '.svg'} alt={name} /></div>)
                 }
 
             </div>
